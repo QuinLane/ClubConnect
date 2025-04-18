0891fe8e31033203de61e6f3551ea9c210185e97
quin <quin@lanewho.com>
2025-04-17 19:05:07 -0600

    fixed failed to fetch issue

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ import formRoutes from "./routes/forms.js";
 import announcementRoutes from "./routes/announcements.js";
 import messageRoutes from "./routes/messages.js";
 import venueRoutes from "./routes/venues.js";
+import userRoutes from "./routes/users.js";
 // Load environment variables
 // dotenv.config();
 
@@ -18,8 +19,13 @@ const app = express();
 const prisma = new PrismaClient();
 const port = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: "http://localhost:5173", // Your frontend’s origin
+  credentials: true, // Allow cookies, tokens, etc.
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
@@ -30,6 +36,7 @@ app.use("/api/forms", formRoutes);
 app.use("/api/announcements", announcementRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/venues", venueRoutes);
+app.use("/api/users", userRoutes);
 
 // Root endpoint
 app.get("/", (req, res) => {
